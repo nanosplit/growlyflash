fa651d66a8ed46d1ec94efd2d665e369ed0ae337
Jacob Montgomery <nanosplit@users.noreply.github.com>
2018-07-17 00:30:07 -0500

    wrap string in parentheses

--- lib/growlyflash.rb
+++ lib/growlyflash.rb
@@ -1,6 +1,6 @@
 require 'growlyflash/version'
 require 'growlyflash/controller_additions'
-require 'growlyflash/engine' if defined?(Rails)
+require 'growlyflash/engine' if (defined?(Rails))
 
 module Growlyflash
 end
